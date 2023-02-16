7c15eb7c2eb2da8244ae99b58ac47905ecb32ffa
ndicz <hnrmanesi@gmail.com>
2023-02-16 16:20:08 +0700

    Trigger Update

--- SP_RestoMemus.sql
+++ SP_RestoMemus.sql
@@ -1,6 +1,31 @@
-﻿c
-CREATE PROCEDURE FindAllRestoMenus
+﻿CREATE SEQUENCE seq_order_number
+START WITH 1
+INCREMENT BY 1;
+
+CREATE TRIGGER tr_order_menus_insert
+ON Resto.order_menus
+INSTEAD OF INSERT
 AS
 BEGIN
-	select * from resto.resto_menus ORDER BY reme_id;
+  DECLARE @prefix nvarchar(20) = 'MENUS#'
+  DECLARE @date nvarchar(20) = CONVERT(nvarchar(8), GETDATE(), 112)
+  DECLARE @next_order_number int
+
+  -- Get the next order number from the sequence
+  SELECT @next_order_number = NEXT VALUE FOR seq_order_number
+
+  -- Insert the rows with the updated order numbers
+  INSERT INTO Resto.order_menus (orme_order_number, orme_order_date, orme_total_item, orme_total_discount, orme_total_amount, orme_pay_type, orme_cardnumber, orme_is_paid, orme_modified_date, orme_user_id)
+  SELECT 
+    @prefix + @date + '-' + RIGHT(REPLICATE('0', 4) + CAST(@next_order_number AS nvarchar(4)), 4), 
+    orme_order_date, 
+    orme_total_item, 
+    orme_total_discount, 
+    orme_total_amount, 
+    orme_pay_type, 
+    orme_cardnumber, 
+    orme_is_paid, 
+    orme_modified_date, 
+    orme_user_id
+  FROM inserted
 END
--- SqlQuery_1.sql
+++ SqlQuery_1.sql
@@ -1,6 +1,18 @@
 ﻿SELECT DATA_TYPE, column_name, IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'resto_menus';
 
-select * from resto.resto_menu_photos;
+select * from resto.order_menus;
+
+ALTER TABLE Resto.resto_menus
+ALTER COLUMN reme_type nvarchar(20);
+
+INSERT INTO Resto.order_menus (orme_order_date, orme_total_item, orme_total_discount, 
+    orme_total_amount, orme_pay_type, orme_cardnumber, orme_is_paid, orme_modified_date, orme_user_id)
+VALUES ('2023-02-16', 3, 5000, 25000, 'CR', '87654', 'P', '2023-02-16', 3)
+
+ALTER TABLE Resto.order_menus
+ALTER COLUMN orme_order_number nvarchar(55) NULL
+
+
 
 SELECT DATA_TYPE, column_name, IS_NULLABLE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'resto_menu_photos';
 
